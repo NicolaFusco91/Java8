45a6318a3966da5e25250f5fa225b509a26f87f8
Nicola <nicola.fusco@finksolution.com>
2020-08-14 10:45:05 +0200

    Aggiuinti commenti e pulizia codice

--- src/esempi/Operatore_ForEach.java
+++ src/esempi/Operatore_ForEach.java
@@ -5,44 +5,57 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.function.Consumer;
 
-public class Iteratore_ForEach {
+/*
+ * 
+ * In questa classe vengono mostrate le diverse forme per iterare una collection
+ * 1 Con iterator classico
+ * 2 forEach con lambda
+ * 3 forEach con classe implementata (utile nel caso bisogna replicare la funzione
+ * 4 forEach con classe anonima
+ */
+
+public class Operatore_ForEach {
+	
 public static void main(String[] args) {
 		
-		//creating sample Collection
+		//Creiamo una semplice lista di interi
 		List<Integer> myList = new ArrayList<Integer>();
+		//Inseriamo i numeri da 0 a 9
 		for(int i=0; i<10; i++) myList.add(i);
 		
-		//traversing using Iterator
+		//Oltre al classico ciclo for, possiamo utilizzare l'Iterator
 		Iterator<Integer> it = myList.iterator();
 		while(it.hasNext()){
 			Integer i = it.next();
-			System.out.println("Iterator Value::"+i);
+			System.out.println("Iterator::"+i);
 		}
 		
-		//traversing through forEach method of Iterable with anonymous class
-		myList.forEach(new Consumer<Integer>() {
-
-			public void accept(Integer t) {
-				System.out.println("forEach anonymous class Value::"+t);
-			}
-
-		});
+		////Utilizzando il metodo forEach attraverso l'operatore lambda
+		myList.forEach(numero -> System.out.println("forEach con lambda::"+numero));
 		
-		//traversing with Consumer interface implementation
-		MyConsumer action = new MyConsumer();
+		//Utilizzando il metodo forEach ed implementanto la functional Interface
+		OperatorePerInteri action = new OperatorePerInteri();
 		myList.forEach(action);
 		
-		myList.forEach(numero -> System.out.println("forEach a modo mio class Value::"+numero));
+		//Utilizzando il metodo forEach di Iterable con l'utilizzo 
+		//di una classe anonima Consumer.
+		myList.forEach(new Consumer<Integer>() {
+			public void accept(Integer t) {
+				System.out.println("forEach classe anonima::"+t);
+			}
+		});
 		
+
 	}
 
 }
 
-//Consumer implementation that can be reused
-class MyConsumer implements Consumer<Integer>{
+//Classe che implementa la functional Interface Consumer in modo da poter 
+// essere riutilizzata
+class OperatorePerInteri implements Consumer<Integer>{
 
 	public void accept(Integer t) {
-		System.out.println("Consumer impl Value::"+t);
+		System.out.println("OperatorePerInteri::"+t);
 	}
 
 
